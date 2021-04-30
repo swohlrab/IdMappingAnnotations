0a470281fa04e6152b677ee5394d02fc8ca70c35
Sylke Wohlrab <swohlrab@ohif01m003.local>
2021-04-30 09:48:15 +0200

    Help pages for functions added

--- R/brite_ids.R
+++ R/brite_ids.R
@@ -2,7 +2,7 @@
 #'
 #'Just type "brite_ids()" to get brite ids and names of all hierachical files in kegg
 #'
-#' @param ()
+#' @param () no need to add something here, just ()
 #' @return kegg brite ids
 #' @export
 brite_ids <- function() {as.data.frame(do.call(rbind,strsplit(readLines('http://rest.kegg.jp/list/brite'),'\t',fixed=T)))}
